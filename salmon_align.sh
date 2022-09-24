1969fcbab40002a32015d4ce08fbc73437ae8069
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-09-24 10:06:39 -0300

    REdiscoverTE

---
+++ salmon_align.sh
@@ -0,0 +1,32 @@
+#Create Salmon Index
+#zcat original/REdiscoverTE/rollup_annotation/REdiscoverTE_whole_transcriptome_hg38-20/*.fa.gz > genome.fasta
+
+#salmon index \
+#	-t genome.fasta \
+#	--threads 64 \
+#	-i REdiscoverTE
+
+#Align Samples to Salmon Index (single-end)
+#for f in /media/labdros/Daniela/srr_cispla/reads/*.fastq ; do
+
+#	echo $f
+
+#	base=${f%.fastq}
+#	echo $base
+
+#	/media/labdros/Daniela/salmon-1.9.0_linux_x86_64/bin/salmon quant --seqBias --gcBias \
+#		--index /media/labdros/Daniela/REdiscoverTE/REdiscoverTE \
+#		--libType A --unmatedReads ${f} \
+#		--validateMappings \
+#		-o ${base}.salmon.REdiscoverTE \
+#		--threads 8
+
+#done
+
+#Rollup / Aggregate Alignments to RE repName
+echo -e "sample\tquant_sf_path" > /media/labdros/Daniela/srr_cispla/reads/REdiscoverTE.tsv
+ls -1 /media/labdros/Daniela/srr_cispla/reads/*.salmon.REdiscoverTE/*_quant.sf | awk -F/ '{split($8,a,".");print a[1]"\t"$0}' >> /media/labdros/Daniela/srr_cispla/reads/REdiscoverTE.tsv
+
+/media/labdros/Daniela/REdiscoverTE/rollup.R --metadata=/media/labdros/Daniela/srr_cispla/reads/REdiscoverTE.tsv --datadir=/media/labdros/Daniela/REdiscoverTE/original/REdiscoverTE/rollup_annotation/ --nozero --threads=8 --assembly=hg38 --outdir=/media/labdros/Daniela/srr_cispla/reads/REdiscoverTE_rollup/
+
+/mnt/d/REdiscoverTE/rollup.R --metadata=/mnt/d/srr_cispla/reads/REdiscoverTE.tsv --datadir=/mnt/d/REdiscoverTE/original/REdiscoverTE/rollup_annotation/ --nozero --threads=8 --assembly=hg38 --outdir=/mnt/d/srr_cispla/reads/REdiscoverTE_rollup/
