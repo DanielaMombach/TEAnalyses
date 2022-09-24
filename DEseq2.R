be9b9edcc8cb3bf4ee853612723992dd04f63b45
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-09-24 11:03:14 -0300

    Create DEseq2.R

---
+++ DEseq2.R
@@ -0,0 +1,23 @@
+BiocManager::install("DESeq2", force = TRUE)
+
+library(DESeq2)
+
+##Input count table
+counttable = read.csv("A2780_all_repFamily_1_raw_counts.csv", header = TRUE, row.names = 1, sep = ",")
+head(counttable)
+
+##The dim must has only columns representing the samples
+dim(counttable)
+
+##colData creation
+condition = c("control","control","control","treatment","treatment","treatment")
+colData = data.frame(row.names=colnames(counttable), treatment=factor(condition, levels=c("control","treatment")))
+colData                    
+
+##Differential expression analysis
+dataset <- DESeqDataSetFromMatrix(countData = counttable, colData = colData, design = ~treatment)
+dataset
+dds = DESeq(dataset)
+head(dds)
+result = results(dds, contrast=c("treatment","control"))
+write.table(result, file="A2780xA2780t_deseq.csv", sep = ",")
