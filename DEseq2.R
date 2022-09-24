7e3a24db56bd0211718049326dfd62dee71bc993
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-09-24 11:22:44 -0300

    rm contrast

--- DEseq2.R
+++ DEseq2.R
@@ -3,7 +3,7 @@ BiocManager::install("DESeq2", force = TRUE)
 library(DESeq2)
 
 ##Input count table
-counttable = read.csv("A2780_all_repFamily_1_raw_counts.csv", header = TRUE, row.names = 1, sep = ",")
+counttable = read.csv("A2780_all_repFamily_raw_counts.csv", header = TRUE, row.names = 1, sep = ",")
 head(counttable)
 
 ##The dim must has only columns representing the samples
@@ -12,12 +12,12 @@ dim(counttable)
 ##colData creation
 condition = c("control","control","control","treatment","treatment","treatment")
 colData = data.frame(row.names=colnames(counttable), treatment=factor(condition, levels=c("control","treatment")))
-colData                    
+colData      
 
 ##Differential expression analysis
-dataset <- DESeqDataSetFromMatrix(countData = counttable, colData = colData, design = ~treatment)
+dataset <- DESeqDataSetFromMatrix(countData = round(counttable), colData = colData, design = ~treatment)
 dataset
 dds = DESeq(dataset)
 head(dds)
-result = results(dds, contrast=c("treatment","control"))
+result = results(dds)
 write.table(result, file="A2780xA2780t_deseq.csv", sep = ",")
