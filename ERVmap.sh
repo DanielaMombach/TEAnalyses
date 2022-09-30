2394230b63409b6c9971724f91f229317f4fa27e
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-09-30 10:25:11 -0300

    Create ERVmap.sh

---
+++ ERVmap.sh
@@ -0,0 +1,7 @@
+#HG38 primary assembly fasta	ensembl	ref/Bowtie2_genome/genome.fa AND ref/bwa_genome/genome.fa
+#HG38 comprehensive gene annotation (gtf)	ensembl	ref/genes.gtf
+
+BWA index files: bwa index -p ref/bwa_genome/genome ref/bwa_genome/genome.fa
+Bowtie index files: bowtie2-build ref/Bowtie2_genome/genome.fa ref/Bowtie2_genome/genome
+
+bash ERVmap_auto.sh /media/labdros/Daniela/srr_cispla/reads
