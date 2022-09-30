d2087987097d223e09a6d2f6e104d4992e7d5ef5
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-09-30 10:51:06 -0300

    conda

--- ERVmap.sh
+++ ERVmap.sh
@@ -4,4 +4,5 @@
 BWA index files: bwa index -p ref/bwa_genome/genome ref/bwa_genome/genome.fa
 Bowtie index files: bowtie2-build ref/Bowtie2_genome/genome.fa ref/Bowtie2_genome/genome
 
+conda activate dani
 bash ERVmap_auto.sh /media/labdros/Daniela/srr_cispla/reads
