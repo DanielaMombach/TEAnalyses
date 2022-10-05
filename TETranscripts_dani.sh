f93a5971139ac4bacfc8a4ea1d2a9e904ad56fca
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-10-05 11:17:50 -0300

    Update TETranscripts_dani.sh

--- TETranscripts_dani.sh
+++ TETranscripts_dani.sh
@@ -1,3 +1,6 @@
+#TEtranscripts and TEcount takes RNA-seq (and similar data) and annotates reads to both genes & transposable elements. TEtranscripts then performs differential analysis using DESeq2.
+
+
 #usage: TEtranscripts -t treatment sample [treatment sample ...]
 #                     -c control sample [control sample ...]
 #                     --GTF genic-GTF-file
@@ -13,17 +16,9 @@
 #  --TE TE-GTF-file      GTF file for transposable element annotations
 
 #If BAM files are unsorted, or sorted by queryname:
+
+#A2780 vs A2780 cisplatin
 /usr/local/bin/TEtranscripts --format BAM --mode multi -t SRR14310043.fastq_Aligned.out.bam SRR14310044.fastq_Aligned.out.bam SRR14310045.fastq_Aligned.out.bam -c SRR14310037.fastq_Aligned.out.bam SRR14310038.fastq_Aligned.out.bam SRR14310039.fastq_Aligned.out.bam --GTF /media/labdros/Daniela/gene_GRCh38.gtf --TE  /media/labdros/Daniela/GRCh38_GENCODE_rmsk_TE.gtf
 
-#usage: TEcount -b RNAseq BAM
-#               --GTF genic-GTF-file
-#               --TE TE-GTF-file
-#               [optional arguments]
-#
-#Required arguments:
-#  -b | --BAM alignment-file  RNAseq alignment file (BAM preferred)
-#  --GTF genic-GTF-file       GTF file for gene annotations
-#  --TE TE-GTF-file           GTF file for transposable element annotations
-
-#If BAM files are unsorted, or sorted by queryname:
-TEcount --format BAM --mode multi -b RNAseq.bam
+#Acis vs Acis cisplatin
+/usr/local/bin/TEtranscripts --format BAM --mode multi -t SRR14310046.fastq_Aligned.out.bam SRR14310047.fastq_Aligned.out.bam SRR14310048.fastq_Aligned.out.bam -c SRR14310040.fastq_Aligned.out.bam SRR14310041.fastq_Aligned.out.bam SRR14310042.fastq_Aligned.out.bam --GTF /media/labdros/Daniela/gene_GRCh38.gtf --TE  /media/labdros/Daniela/GRCh38_GENCODE_rmsk_TE.gtf
