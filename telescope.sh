54735fa08513c7ea9f225d9dc00ed2fa8c80027d
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-10-17 10:45:00 -0300

    Update telescope.sh

--- telescope.sh
+++ telescope.sh
@@ -11,4 +11,37 @@ telescope test #only works without bulk
 telescope assign #with file paths shown above
 
 #run for each SRR
-telescope assign SRR14310037.fastq_Aligned.out.bam /media/labdros/Daniela/pcbi.1006453.s006.gtf 
+telescope assign SRR14310037.fastq_Aligned.out.bam /media/labdros/Daniela/pcbi.1006453.s006.gtf
+
+#get same ids in all
+#get the ids
+cut -f1 telescope-telescope_report_37.tsv telescope-telescope_report_38.tsv telescope-telescope_report_39.tsv telescope-telescope_report_43.tsv telescope-telescope_report_44.tsv telescope-telescope_report_45.tsv | sed 's/"//g' | sort | uniq -c | sed 's/^[ \t]*//;s/ /\t/' | sort -k1 -h | grep ^6 | cut -f2 > em_todos.txt
+
+#get ids and counts
+  for tab in telescope-telescope_report_37.tsv telescope-telescope_report_38.tsv telescope-telescope_report_39.tsv telescope-telescope_report_43.tsv telescope-telescope_report_44.tsv telescope-telescope_report_45.tsv; do grep -w -f em_todos.txt $tab | cut -f1,3 | sed 's/\t/,/' > counts_${tab}; done
+
+#conferir
+for tab in telescope-telescope_report_37.tsv telescope-telescope_report_38.tsv telescope-telescope_report_39.tsv telescope-telescope_report_43.tsv telescope-telescope_report_44.tsv telescope-telescope_report_45.tsv; do grep -c -f em_todos.txt $tab ;done
+
+#DESeq2
+library(DESeq2)
+
+##Input count table
+counttable = read.csv("A2780_counts.csv", header = TRUE, row.names = 1, sep = ",")
+head(counttable)
+
+##The dim must has only columns representing the samples
+dim(counttable)
+
+##colData creation
+condition = c("control","control","control","treatment","treatment","treatment")
+colData = data.frame(row.names=colnames(counttable), treatment=factor(condition, levels=c("control","treatment")))
+colData      
+
+##Differential expression analysis
+dataset <- DESeqDataSetFromMatrix(countData = round(counttable), colData = colData, design = ~treatment)
+dataset
+dds = DESeq(dataset)
+head(dds)
+result = results(dds)
+write.table(result, file="A2780xA2780t_deseq.csv", sep = ",")
