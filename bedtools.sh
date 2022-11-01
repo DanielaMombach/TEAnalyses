45676392ec9996b58affed5e00723ea915485de4
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-11-01 11:13:57 -0300

    Update bedtools.sh

--- bedtools.sh
+++ bedtools.sh
@@ -9,12 +9,12 @@ awk '$3 == "gene"' GCF_000001405.40_GRCh38.p14_genomic.gtf | awk '{print $1, $4,
 ### upstream region
 bash create_upstream_region.sh GCF_000001405.40_GRCh38.p14_genomic_gene.bed 3000 upstream_genes.bed
 
-##
+## TEtranscripts
 awk '$3 == "CDS"' gene_GRCh38.gtf | awk '{print $4, $5, $1, $7, $7}' > gene_GRCh38.txt
 awk '$3 == "CDS"' gene_GRCh38.gtf | cut -f9 | cut -d';' -f1 | cut -d'"' -f2 > gene_GRCh38_ID.txt
 paste gene_GRCh38_ID.txt gene_GRCh38.txt > gene_GRCh38.bed 
 sed 's/ /\t/g' gene_GRCh38.bed -i
-bash create_upstream_region.sh gene_GRCh38.bed 3000 upstream_genes.bed
+bash create_upstream_region.sh gene_GRCh38.bed 5000 upstream_genes2.bed
 
 #create_upstream_region.sh
 #!/bin/bash
@@ -49,19 +49,26 @@ done < $1
 ## TE file to BED (remove header and C to -)
 tail -n +4 GCF_000001405.40_GRCh38.p14_rm.out | awk '{print $5, $6, $7, $10, $15, $9}' | sed 's/C$/-/;s/ /\t/g' > GCF_000001405.40_GRCh38.p14_rm.bed
 
-##
+## TEtranscripts
 awk '{print $4, $5, $1, $7, $7}' GRCh38_GENCODE_rmsk_TE.gtf > GRCh38_GENCODE_rmsk_TE.txt
 cut -d';' -f1 GRCh38_GENCODE_rmsk_TE.gtf | cut -d'"' -f2 > GRCh38_GENCODE_rmsk_TE_ID.txt
 paste GRCh38_GENCODE_rmsk_TE_ID.txt GRCh38_GENCODE_rmsk_TE.txt > GRCh38_GENCODE_rmsk_TE.bed 
 sed 's/ /\t/g' GRCh38_GENCODE_rmsk_TE.bed -i
 
+## telescope
+awk '{print $4, $5, $1, $7, $7}' pcbi.1006453.s006.gtf > pcbi.1006453.s006.txt
+cut -d';' -f2 pcbi.1006453.s006.gtf | cut -d'"' -f2 > pcbi.1006453.s006_ID.txt
+paste pcbi.1006453.s006_ID.txt pcbi.1006453.s006.txt > pcbi.1006453.s006.bed
+sed 's/ /\t/g' pcbi.1006453.s006.bed -i
+sed '/#/d' pcbi.1006453.s006.bed -i
+
 # run bedtools intersect
 bedtools intersect -wa -wb -a GCF_000001405.40_GRCh38.p14_genomic_gene.bed -b GCF_000001405.40_GRCh38.p14_rm.bed -s -f 0.2 > TEs_inside_gene.txt
 bedtools intersect -wa -wb -a upstream_genes.bed -b GCF_000001405.40_GRCh38.p14_rm.bed -s -f 0.2 > TEs_upstream_gene.txt
 
-# 
-bedtools intersect -wa -wb -a gene_GRCh38.bed -b GRCh38_GENCODE_rmsk_TE.bed -s -f 0.2 > TEs_inside_gene.txt
-bedtools intersect -wa -wb -a upstream_genes.bed -b GRCh38_GENCODE_rmsk_TE.bed -s -f 0.2 > TEs_upstream_gene.txt
+# TE transcripts
+bedtools intersect -wa -wb -a gene_GRCh38.bed -b GRCh38_GENCODE_rmsk_TE.bed -s -f 0.2 > TEs_inside_gene2.txt
+bedtools intersect -wa -wb -a upstream_genes2.bed -b GRCh38_GENCODE_rmsk_TE.bed -s -f 0.2 > TEs_upstream_gene2.txt
 
 grep -f A2780_ids.txt TEs_inside_gene.txt
 grep -f A2780_ids.txt TEs_upstream_gene.txt
