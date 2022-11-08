c41600a10435b03211332077ce33f4143f6215f0
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-11-08 09:09:57 -0300

    Update bedtools.sh

--- bedtools.sh
+++ bedtools.sh
@@ -20,36 +20,6 @@ paste gencode.v42.annotation_alm.txt gencode.v42.annotation_end.txt > gencode.v4
 sed 's/ /\t/g' gencode.v42.annotation_done.txt > gencode.v42.annotation.bed
 bash create_upstream_region.sh gencode.v42.annotation.bed 5000 gencode.v42.annotation_5kbupstream.bed
 
-#create_upstream_region.sh
-#!/bin/bash
-
-#Author: Daniel Siqueira de Oliveira, feb/2022
-
-#$1= input.bed
-#$2= window size
-#$3= output.bed
-
-while IFS= read -r line
-do
-	window_size=$2        
-	minimum=1
-        scaff=$(cut -f1 <<< "$line")
-        Start=$(cut -f2 <<< "$line")
-        end=$(cut -f3 <<< "$line")
-	final=$(cut -f4-6 <<< "$line")
-        fita=$(cut -f6 <<< "$line")
-        if [ "$fita" == + ]; then
-                upstream=$(($Start - $window_size))
-                        if [ "$upstream" -lt "$minimum" ]; then
-                        upstream=1
-                        fi
-                echo -e "$scaff"'\t'"$upstream"'\t'"$Start"'\t'"$final" >> $3
-                else
-                upstream=$(($end + $window_size))
-                echo -e "$scaff"'\t'"$end"'\t'"$upstream"'\t'"$final" >> $3
-        fi
-done < $1
-
 ## TE file to BED (remove header and C to -)
 tail -n +4 GCF_000001405.40_GRCh38.p14_rm.out | awk '{print $5, $6, $7, $10, $15, $9}' | sed 's/C$/-/;s/ /\t/g' > GCF_000001405.40_GRCh38.p14_rm.bed
 
