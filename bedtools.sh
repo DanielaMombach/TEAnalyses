a3ebd51df42ba91f70f1744e15e15e1e225b9bc1
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-12-07 10:16:08 -0300

    Update bedtools.sh

--- bedtools.sh
+++ bedtools.sh
@@ -6,13 +6,13 @@ annot2BED.ipynb
 bash create_upstream_region.sh gencode.v36.annotation.bed 5000 gencode.v36.annotation_5kbupstream.bed
 
 # telescope
-bedtools intersect -wa -wb -a gencode.v36.annotation.bed -b pcbi.1006453.s006.bed -s -f 0.2 > TEs_inside_gene_tel.txt
-bedtools intersect -wa -wb -a gencode.v36.annotation_5kbupstream.bed -b pcbi.1006453.s006.bed -s -f 0.2 > TEs_upstream_gene_tel.txt
+bedtools intersect -wa -wb -a gencode.v36.annotation_python.bed -b pcbi.1006453.s006_python.bed -s -f 0.2 > TEs_inside_gene_tel.txt
+bedtools intersect -wa -wb -a gencode.v36.annotation_5kbupstream.bed -b pcbi.1006453.s006_python.bed -s -f 0.2 > TEs_upstream_gene_tel.txt
 
 ## REdiscoverTE
 # rediscoverBED.py = plus and minus bed files
-bedtools intersect -wa -wb -a gencode.v36.annotation.bed -b  rmsk_plus.bed -s -f 0.2 > TEs_inside_gene_re.txt
-bedtools intersect -wa -wb -a gencode.v36.annotation.bed -b  rmsk_min.bed -s -f 0.2 > TEs_inside_gene_rem.txt
+bedtools intersect -wa -wb -a gencode.v36.annotation_python.bed -b  rmsk_plus.bed -s -f 0.2 > TEs_inside_gene_re.txt
+bedtools intersect -wa -wb -a gencode.v36.annotation_python.bed -b  rmsk_min.bed -s -f 0.2 > TEs_inside_gene_rem.txt
 bedtools intersect -wa -wb -a gencode.v36.annotation_5kbupstream.bed -b  rmsk_plus.bed -s -f 0.2 > TEs_upstream_gene_re.txt
 bedtools intersect -wa -wb -a gencode.v36.annotation_5kbupstream.bed -b  rmsk_min.bed -s -f 0.2 > TEs_upstream_gene_rem.txt
 
