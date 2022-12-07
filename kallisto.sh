e22f659ade6ddbbe16f37fafdaa2f08d3955b855
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-12-07 10:26:20 -0300

    Update kallisto.sh

--- kallisto.sh
+++ kallisto.sh
@@ -1,4 +1,5 @@
-#index
+# kallisto ##############################################################################################################
+# index
 ln -s /home/projects2/databases/gencode/release36/gencode.v36.transcripts.fa.gz
 kallisto index -i geneSeq_v36.idx gencode.v36.transcripts.fa.gz 2>log_indexKalisto
     
@@ -18,7 +19,7 @@ kallisto quant -i geneSeq_v36.idx -o kallisto/output_46 --single -l 76 -s 1 -t 3
 kallisto quant -i geneSeq_v36.idx -o kallisto/output_47 --single -l 76 -s 1 -t 3 SRR14310047_SS.fastq.gz &
 kallisto quant -i geneSeq_v36.idx -o kallisto/output_48 --single -l 76 -s 1 -t 3 SRR14310048_SS.fastq.gz &
 
-#TX2gene
+# TX2gene #############################################################################################################
 # Create mapping between transcripts and corresponding genes
 echo "TXNAME,GENEID" > tx2gene.csv
 awk -F "|" '{if(NR>1){print $1","$2}}' kallisto/output_37/abundance.tsv >> tx2gene.csv
@@ -28,22 +29,22 @@ for f in $(find . -type d -name "output*"); do
     awk -F "|" '{print $1$9}' $f/abundance.tsv > $f/final_abundance.tsv
 done
 
-#TXimport in R
+# TXimport in R ########################################################################################################
 setwd("/media/labdros/Daniela/srr_cispla")
 
-#First, we locate the directory containing the files. 
+# First, we locate the directory containing the files. 
 dir <- "/media/labdros/Daniela/srr_cispla/kallisto"
 list.files(dir)
 
-#Next, we create a named vector pointing to the quantification files. 
+# Next, we create a named vector pointing to the quantification files. 
 samples <- read.table(file.path(dir, "samples.txt"), header = TRUE)
 samples
 
-#create tx2gene (https://hbctraining.github.io/DGE_workshop_salmon/lessons/01_DGE_setup_and_overview.html) - ENSEMBL
+# create tx2gene (https://hbctraining.github.io/DGE_workshop_salmon/lessons/01_DGE_setup_and_overview.html) - ENSEMBL
 tx2gene <- read.delim("tx2gene_grch38_ens94.txt")
 tx2gene
 
-#kallisto
+# for kallisto
 library(tximport)
 files <- file.path(dir, samples$run, "abundance.tsv")
 files
@@ -53,17 +54,17 @@ txi.kallisto.tsv <- tximport(files, type = "kallisto", tx2gene = tx2gene, ignore
 head(txi.kallisto.tsv$counts)
 write.table(txi.kallisto.tsv, file="A2780_txi.csv", sep = ",")
 
-#DESeq2
+# DESeq2 in R #############################################################################################################
 library(DESeq2)
 
-##Input count table
+## Input count table
 counttable = read.csv("A2780_kallisto_counts.csv", header = TRUE, row.names = 1, sep = ",")
 head(counttable)
 
-##The dim must has only columns representing the samples
+## The dim must has only columns representing the samples
 dim(counttable)
 
-##colData creation
+## colData creation
 condition = c("control","control","control","treatment","treatment","treatment")
 colData = data.frame(row.names=colnames(counttable), treatment=factor(condition, levels=c("control","treatment")))
 colData      
@@ -76,11 +77,10 @@ counttable$counts.sample5 = as.numeric(counttable$counts.sample5)
 counttable$counts.sample6 = as.numeric(counttable$counts.sample6)
 counttable[is.na(counttable)] <- 0
 
-##Differential expression analysis
+## Differential expression analysis
 dataset <- DESeqDataSetFromMatrix(countData = round(counttable), colData = colData, design = ~treatment)
 dataset
 dds = DESeq(dataset)
 head(dds)
 result = results(dds)
 write.table(result, file="A2780_kallisto_deseq.csv", sep = ",")
-
